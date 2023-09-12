3a5462172da793c9eab11167cb0ec11f106a1983
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-09-12 16:56:31 -0400

    adicionando css da pagina principal

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import './login.css'
+import Image from 'next/image';
 
 export default function Login() {
   const [user, setUser] = useState({
@@ -25,20 +27,31 @@ export default function Login() {
     }
   }
   return (
-    <div>
+    <div className="principal">
+
+
+                <Image
+                    src="/background.jpg"
+                    alt="background image"
+                    fill
+                />
+
+
       <h1>Login</h1>
       <form onSubmit={handlerLogin}>
-        <div >
+        <div className="inpts" >
+        <label for="">E-mail</label>
         <input type="email"
           onChange={(e) => { setUser({ ...user, email: e.target.value }) }}
         />
-        <label for="">E-mail</label>
+        
         </div>
-        <div>
+        <div className="inpts" >
+       <label for="">Senha</label> 
         <input type='password'
           onChange={(e) => { setUser({ ...user, password: e.target.value }) }}
         />
-        <label for="">Senha</label>
+        
         </div>
           <a>
           <button>Entrar</button>
