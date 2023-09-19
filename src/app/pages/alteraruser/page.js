2b2e74f1b5ddfa52b822d79b9f3bd7b30bd80855
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-09-19 10:31:53 -0300

    fim de modificacoes pt-2

--- src/app/pages/alteraruser/page.js
+++ src/app/pages/alteraruser/page.js
@@ -1,44 +1,44 @@
 'use client'
-
-import 'react-toastify/dist/ReactToastify.css';
-import '../styles/register.css';
-import Nav from "../components/Navbar";
 import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/register.css'
+import Nav from '../components/Navbar'
+
+export default function Login() {
 
 
-export default async function Alterar() {
-
-   const enviar = (e) => {
+  const alterar = (e) => {
     e.preventDefault();
-    toast.success('Usuário alterado com sucesso')
+    toast.success('Usuário registrado com sucesso');
+
    }
 
-    return (
-        <div className="tudo">
-            <Nav/>
-        <div className="principal card">
-        <h1>Alterar Usuário</h1>
-        <form onSubmit={enviar}>
+  return (
+    <div className="tudo">
+      <Nav/>
+      <div className="principal card">
+      <h1>Alterar Usuário</h1>
+      <form onSubmit={alterar}>
+      <div className="inpts" >
+       <label for="">Novo Nome</label> 
+        <input type='text' placeholder="John Doe"/>
+        
+        </div>
+         
         <div className="inpts" >
-         <label for="">Novo Nome</label> 
-          <input id="nome" type='text' placeholder="John Doe"/>
-          
-          </div>
-          <div className="inpts" >
-          <label for="" >Novo E-mail</label>
-          <input id='mail' type="email" placeholder="johndoe@gmail.com" />
-          
-          </div>
-          <div className="inpts" >
-         <label for="">Nova Senha</label> 
-          <input id='passwd' type='password' placeholder="*******"/>
-          
-          </div>
-           
-            <button className="entrar-button">Alterar</button>
-        </form>
-        <ToastContainer toastStyle={{backgroundColor:"#030029"}} />
-      </div>
-      </div>
-    );
-};
+        <label for="" >Novo E-mail</label>
+        <input type="email" placeholder="johndoe@gmail.com"/>
+        
+        </div>
+        <div className="inpts" >
+       <label for="">Nova Senha</label> 
+        <input type='password' placeholder="*******"/>
+        
+        </div>
+          <button className="entrar-button">Entrar</button>
+      </form>
+      <ToastContainer toastStyle={{backgroundColor:"#030029"}} />
+    </div>
+    </div>
+  )
+}
