8f7b420907b5f9201ceb1ac910b3733edb0f30b1
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-10-17 15:51:45 -0400

    Verfificando se o usuario está autenticado

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -2,7 +2,18 @@
 
 const url = "https://aula-17-10-eight.vercel.app/"
 
+
+
 const getUserAuthenticated = async (user) => {
+
+    const responseOfApi = await fetch(url + "/user/authenticate",
+    {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},  //Informando os dados que serão enviados e os tipos de dados que serão enviados
+        body: JSON.stringify(user)
+    }
+    )
+
    // let userAuth = {}
    // for (let i = 0; i < users.length; i++) {
    //     if(users[i].email === user.email && users[i].password === user.password) 
