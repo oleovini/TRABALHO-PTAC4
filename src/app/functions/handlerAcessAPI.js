50dccde216af5d2d661e27a285ee2f39f634bab9
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-10-17 16:13:57 -0400

    alterações

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,37 +1,29 @@
 'use server'
 
-const url = "https://aula-17-10-eight.vercel.app/"
+const url = "https://users-leovini.vercel.app"
 
 
 
 const getUserAuthenticated = async (user) => {
 
-    const responseOfApi = await fetch(url + "/user/authenticate",
+    const responseOfApi = await fetch(url + "/user/authenticated",
     {
         method: "POST",
+        cache:"no-cache",
         headers: {"Content-Type" : "application/json"},  //Informando os dados que serão enviados e os tipos de dados que serão enviados
         body: JSON.stringify(user)
     }
     )
     const useAuth = await responseOfApi.json();
-    return useAuth; 
-   // let userAuth = {}
-   // for (let i = 0; i < users.length; i++) {
-   //     if(users[i].email === user.email && users[i].password === user.password) 
-   //     {
-   //         userAuth=users[i];
-   //     }
-   // }
-
-   // return userAuth;
-
-
+    return useAuth;
 }
 
-const getUsers = (user) =>{
+const getUsers = async (user) =>{
 
+    const responseOfApi = await fetch(url + "/users")
+    const useAuth = await responseOfApi.json();
+    return useAuth;
 
-  //  return users;
 }
 
 export { getUsers, getUserAuthenticated };
