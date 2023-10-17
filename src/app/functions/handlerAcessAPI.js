ee41e7e166536d938ab98e93b56777442b734e11
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-10-17 15:42:54 -0400

    modificando para pegar o usuário do elefante

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,26 +1,25 @@
 'use server'
 
-const users = [
-    { name: 'Leo',  email: 'leo@gmail.com', password: '123', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' },
-    { name: 'Nat', email: 'nat@gmail.com', password: '123', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' },
-    { name: 'Nascimento', email: 'nascimento@gmail.com', password: '123', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' },
-    { name: 'Joao', email: 'joaodasilva@gmail.com', password: '123', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' }
-];
-const getUserAuthenticated = (user) => {
-    let userAuth = {}
-    for (let i = 0; i < users.length; i++) {
-        if(users[i].email === user.email && users[i].password === user.password) 
-        {
-            userAuth=users[i];
-        }
-    }
+const url = "https://aula-17-10-eight.vercel.app/"
 
-    return userAuth;
+const getUserAuthenticated = async (user) => {
+   // let userAuth = {}
+   // for (let i = 0; i < users.length; i++) {
+   //     if(users[i].email === user.email && users[i].password === user.password) 
+   //     {
+   //         userAuth=users[i];
+   //     }
+   // }
+
+   // return userAuth;
+
+
 }
 
 const getUsers = (user) =>{
 
-    return users;
+
+  //  return users;
 }
 
 export { getUsers, getUserAuthenticated };
